57509ce200b01da7a9de688f2031ec34c43250b2
Sukriti Singh <sukriti.singh899@gmail.com>
2024-05-04 18:48:44 +0530

    feat: adds jobs api slice

---
+++ src/store/index.ts
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { jobsApi } from './services/jobs';
+
+export const store = configureStore({
+  reducer: {
+    [jobsApi.reducerPath]: jobsApi.reducer,
+  },
+});
---
+++ src/store/services/jobs.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+import * as CONSTANTS from '../../constants/app';
+
+export const jobsApi = createApi({
+  reducerPath: 'jobsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: CONSTANTS.BASE_URL }),
+  endpoints: (builder) => ({
+    getJobs: builder.query({
+      query: ({ limit = 10, offset = 0 }) => ({
+        url: CONSTANTS.ENDPOINTS.JOBS,
+        method: 'POST',
+        body: {
+          limit,
+          offset,
+        },
+        merge: (currentData, newData) => {
+          currentData.results.push(...newData.results);
+        },
+      }),
+    }),
+  }),
+});
